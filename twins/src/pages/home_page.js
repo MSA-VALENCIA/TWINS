1bfe1c8b90854a08d5e264e044bd0675d3a0c14c
danihp6 <danihp6@gmail.com>
2020-04-08 12:08:32 +0200

    Añadido react native elements y icons

--- twins/src/pages/home_page.js
+++ twins/src/pages/home_page.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import { Text, View } from 'react-native'
+import Icon from 'react-native-vector-icons/FontAwesome5'
 
 function HomePage({navigation}) {
     return (
         <View style={{alignItems:'center',justifyContent:'center'}}>
-        <Text>Hello</Text>
+        <Icon name='user'></Icon>
       </View>
     )
 }
 
 export default HomePage
-
