922613c30895852d456506a820d8dc60c3b7c9e0
Alberto Puerta <puertamillet@gmail.com>
2020-04-13 11:45:20 +0200

    assets

--- twins/src/view/pages/game_page.js
+++ twins/src/view/pages/game_page.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Text, View, ImageBackground } from 'react-native'
 import Icon from 'react-native-vector-icons/FontAwesome5'
 
 function GamePage({ navigation }) {
     return (
-        <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-            <Icon name='gamepad'></Icon>
-        </View>
+        
+        
     )
 }
 
