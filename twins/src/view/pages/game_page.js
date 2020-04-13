8e238f13ee04c19d47e4375330a0b7bdc107a79e
Alberto Puerta <puertamillet@gmail.com>
2020-04-13 12:16:39 +0200

    GamePage fondo

--- twins/src/view/pages/game_page.js
+++ twins/src/view/pages/game_page.js
@@ -1,12 +1,21 @@
 import React from 'react'
 import { Text, View, ImageBackground } from 'react-native'
-import Icon from 'react-native-vector-icons/FontAwesome5'
 
 function GamePage({ navigation }) {
     return (
-        
-        
+        <ImageBackground source={require('../../../assets/images/background.jpg')} style={styles.background}>
+            <View>
+                
+            </View>
+        </ImageBackground>
     )
 }
+
+const styles = StyleSheet.create({
+    background: {
+      flex: 1,
+      flexDirection: 'column',
+    },
+  })
 
 export default GamePage
